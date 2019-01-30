50f1358a63adbd971cd9a21b7e6c9423365e9761
hessaAqeel <hessa1aqeel@gmail.com>
2019-01-30 15:29:59 +0300

    *.*

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Tic Tac Toe
+
+
+
+Tic Tac Toe is X and O game where both players take turn to make a move and compete to win . It's a pretty easy game to win , and you can have different schemes to win it.
+
+ I used a JavaScript function with an if statement to change turns between two players, after the players finish their game there will be either one winner or a toe; where no one wins, to be able to do this I used an if statement to determine the winner and called the function. 
+
+After the game is finished, the user can press a button to relaod the page and start a new game. 
+
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 console.log("Test!");
 
 var gamer = 'X';
+var gamerX = 0;
+var gamerO = 0;
+
+
 function change1() {
 
     $(event.target).text(gamer) // This line allows the X&O to change depends on the place was clicked 
@@ -18,6 +22,7 @@ function change1() {
     theWinner(); // Calling the fuction to show the winner .
 
 
+
 } $('td').on('click', change1);
 
 
@@ -26,51 +31,57 @@ function theWinner() {
 
     if (($('#td1').text() !== '') && ($('#td1').text() === $('#td2').text()) && ($('#td2').text() === $('#td3').text())) {
         $('h2').text($('#td3').text() + ' Wins ');
-        reset();
         stopUserClicks();
+        // if ($('#td3').text() === 'X') {
+        //      gamerX++
+        //  } else {
+        //     gamerO++
+        // }
+
 
     } else if (($('#td4').text() !== '') && ($('#td4').text() === $('#td5').text()) && ($('#td5').text() === $('#td6').text())) {
         $('h2').text($('#td6').text() + ' Wins ');
-        reset();
+
         stopUserClicks();
 
     } else if (($('#td7').text() !== '') && ($('#td7').text() === $('#td8').text()) && ($('#td8').text() === $('#td9').text())) {
         $('h2').text($('#td9').text() + ' Wins ');
-        reset();
+
         stopUserClicks();
 
     } else if (($('#td1').text() !== '') && ($('#td1').text() === $('#td4').text()) && ($('#td4').text() === $('#td7').text())) {
         $('h2').text($('#td7').text() + ' Wins ');
-        reset();
+
         stopUserClicks();
 
     } else if (($('#td2').text() !== '') && ($('#td2').text() === $('#td5').text()) && ($('#td5').text() === $('#td8').text())) {
         $('h2').text($('#td8').text() + ' Wins ');
-        reset();
+
         stopUserClicks();
 
     } else if (($('#td3').text() !== '') && ($('#td3').text() === $('#td6').text()) && ($('#td6').text() === $('#td9').text())) {
         $('h2').text($('#td9').text() + ' Wins ');
-        reset();
+
         stopUserClicks();
 
     } else if (($('#td5').text() !== '') && ($('#td1').text() === $('#td5').text()) && ($('#td5').text() === $('#td9').text())) {
         $('h2').text($('#td9').text() + ' Wins ');
-        reset();
+
         stopUserClicks();
     } else if (($('#td5').text() !== '') && ($('#td3').text() === $('#td5').text()) && ($('#td5').text() === $('#td7').text())) {
         $('h2').text($('#td7').text() + ' Wins ');
-        reset();
+
         stopUserClicks();
 
     } else if (($('#td1').text() !== '') && ($('#td2').text() !== '') && ($('#td3').text() !== '') && ($('#td4').text() !== '') && ($('#td5').text() !== '') && ($('#td6').text() !== '') && ($('#td7').text() !== '') && ($('#td8').text() !== '') && ($('#td9').text() !== '')) {
         $('h2').text(' Tie ');
-        reset();
+
         stopUserClicks();
 
     }
 
 }
+
 function reset() {
     $('td').text('');
 } // To enable the board to clear up after someone wins or after a toe 
@@ -80,3 +91,13 @@ function stopUserClicks() {
     $('td').off('click');
 
 } // After the game ends user can never press any empty cell. 
+
+function reload() {
+    location.reload();
+} // to reload the page after the user clicks on 'play again'button 
+
+
+
+function goPageTwo() {
+    $('.bodyTwo').text();
+} // so user can move to the second page 
